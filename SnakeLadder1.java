6320c87d2b0a988f7f7dbd569e8002ed9a730a32
sayali-2001 <sayalithakare47@gmail.com>
2022-11-27 22:41:36 +0530

    The Player rolls the die

---
+++ SnakeLadder1.java
@@ -0,0 +1,9 @@
+package SnakeLadder;
+
+public class SnakeLadder1 {
+    public static void main(String[] args) {
+        int StartPos = 0;
+        System.out.println("Game Starts Now");
+        System.out.println("Starting position of player is: "+StartPos);
+    }
+}
